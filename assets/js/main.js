1bb8afb3c7dc8f428c77e9cce77e5e957aaa2582
Shubhankar30 <shubhankarranade30@gmail.com>
2018-12-20 22:04:41 +0900

    Add datepicker

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,9 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
 (function($) {
+	
 
 	var	$window = $(window),
 		$body = $('body');
---
+++ assets/js/populate.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    //your code here
+    console.log("ASDASDASD")
+    $('.datepicker').datepicker({
+     format:'dd/mm/yyyy',
+     startView:'1994'
+    });
+
+
+
+  });
