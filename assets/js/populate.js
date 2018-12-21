c8bcf10b9b8b3b5f33fd4c7223ef7d85c267e322
shubhankar <shubhankar@to-on.com>
2018-12-21 10:09:55 +0900

    Add grid

--- assets/js/populate.js
+++ assets/js/populate.js
@@ -1,7 +1,21 @@
+// function that builds a grid in the "container"
+function createGrid(x) {
+  for (var rows = 0; rows < x; rows++) {
+      for (var columns = 0; columns < x; columns++) {
+          $("#container").append("<div class='grid'></div>");
+      };
+  };
+  $(".grid").width(960/x);
+  $(".grid").height(960/x);
+};
+
+
 $(document).ready(function () {
+  createGrid(16);
+
     //your code here
     console.log("ASDASDASD")
-    $('.datepicker').datepicker({
+    $('#datepicker').datepicker({
      format:'dd/mm/yyyy',
      startView:'1994'
     });
